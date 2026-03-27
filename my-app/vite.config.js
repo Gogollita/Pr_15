fbf6a21fc440cec928234feccdc946523dbed947
Suzanna <suzanguseinova@gmail.com>
2026-03-27 03:15:49 +0300

    Updated Vite config

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+import vue from '@vitejs/plugin-vue' 
+export default defineConfig({ 
+plugins: [vue()], 
+base: '/Pr_15/'
 })
